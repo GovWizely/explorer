95c52d11765e648ceef8bb41a5facf906210d187
AJ Singh <ajsingh273@gmail.com>
2015-02-03 11:02:40 -0500

    new styles and more coutnries

--- app/models/country-commercial-guide.js
+++ app/models/country-commercial-guide.js
@@ -6,5 +6,8 @@ export default DS.Model.extend({
   pdf_section: DS.attr('string'),
   section_title: DS.attr('string'),
   content: DS.attr('string'),
+  country: DS.attr('string'),
+  industry: DS.attr('string'),
+  topic: DS.attr('string'), 
   section_url: DS.attr('string')
 });
