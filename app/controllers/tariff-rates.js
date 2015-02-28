22d0a3ad0f7f84ba0b601e586ab911132d2dd18e
James Isaacs <james@govwizely.com>
2015-02-27 16:08:35 -0800

    added new countries

--- app/controllers/tariff-rates.js
+++ app/controllers/tariff-rates.js
@@ -22,9 +22,20 @@ export default Ember.Controller.extend({
 
   sourceList: [
     {value: 'AUSTRALIA', label: 'FTA Australia Tariff Rates'},
+    {value: 'CHILE', label: 'FTA Chile Tariff Rates'},
+    {value: 'BAHRAIN', label: 'FTA Bahrain Tariff Rates'},
+    {value: 'COLOMBIA', label: 'FTA Colombia Tariff Rates'},
     {value: 'COSTA_RICA', label: 'FTA Costa Rica Tariff Rates'},
+    {value: 'DOMINICAN_REPUBLIC', label: 'FTA Dominican Republic Tariff Rates'},
     {value: 'EL_SALVADOR', label: 'FTA El Salvador Tariff Rates'},
     {value: 'GUATEMALA', label: 'FTA Guatemala Tariff Rates'},
-    {value: 'KOREA', label: 'FTA South Korea Tariff Rates'}
+    {value: 'HONDURAS', label: 'FTA Honduras Tariff Rates'},
+    {value: 'MOROCCO', label: 'FTA Morocco Tariff Rates'},
+    {value: 'NICARAGUA', label: 'FTA Nicaragua Tariff Rates'},
+    {value: 'OMAN', label: 'FTA Oman Tariff Rates'},
+    {value: 'PANAMA', label: 'FTA Panama Tariff Rates'},
+    {value: 'PERU', label: 'FTA Peru Tariff Rates'},
+    {value: 'SINGAPORE', label: 'FTA Singapore Tariff Rates'},
+    {value: 'SOUTH_KOREA', label: 'FTA South Korea Tariff Rates'}
   ],
 });
