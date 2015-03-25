ef336d9e7bc3d092eb0aae631a9e6ad6059a826b
James Isaacs <james@govwizely.com>
2015-03-25 10:11:12 -0700

    Added ISA and Part 561 to sources

--- app/controllers/consolidated-screening-list-entries.js
+++ app/controllers/consolidated-screening-list-entries.js
@@ -42,11 +42,13 @@ export default Ember.ArrayController.extend({
     {value: 'EL',  label: 'Entity List (EL) - Bureau of Industry and Security'},
     {value: 'FSE', label: 'Foreign Sanctions Evaders (FSE) - Treasury Department'},
     {value: 'DTC', label: 'ITAR Debarred (DTC) - State Department'},
+    {value: 'ISA', label: 'Non-SDN Iranian Sanctions Act List (ISA) - Treasury Department'},
     {value: 'ISN', label: 'Nonproliferation Sanctions (ISN) - State Department'},
     {value: 'PLC', label: 'Palestinian Legislative Council List (PLC) - Treasury Department'},
     {value: 'SSI', label: 'Sectoral Sanctions Identifications List (SSI) - Treasury Department'},
     {value: 'SDN', label: 'Specially Designated Nationals (SDN) - Treasury Department'},
-    {value: 'UVL', label: 'Unverified List (UVL) - Bureau of Industry and Security'}
+    {value: 'UVL', label: 'Unverified List (UVL) - Bureau of Industry and Security'},
+    {value: '561', label: 'Part 561 List (561) - Treasury Department'}
   ],
 
   countryList: [
