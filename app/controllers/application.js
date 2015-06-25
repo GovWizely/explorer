c6804053e0911ea77edb0f02233af07eaabccbdb
Calum Halcrow <calum@rrsoft.co>
2015-06-25 12:32:07 +0800

    Remove ECCNs from nav for the time-being.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -15,7 +15,7 @@ export default Ember.Controller.extend({
     { label: "FAQs on Exporting", value: "ita-faqs"},
     { label: "ITA Offices & Centers", value: "ita-office-locations"},
     { label: "Trade News & Articles", value: "sharepoint-trade-articles"},
-    { label: "ECCNs", value: "eccns"},
+    //{ label: "ECCNs", value: "eccns"},
     { label: "Zip Code to USEAC", value: "ita-zip-codes"},
     { label: "Business Service Providers", value: "business-service-providers"},
     { label: "Environmental Solutions", value: "environmental-solutions"}
