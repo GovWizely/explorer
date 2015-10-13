b6bb292ad5d1f04745c6f0ddf40ce998cb74a05f
Victor Lim <victor@rrsoft.co>
2015-10-13 22:45:28 +0800

    change country fact sheet source from array to string

--- app/models/country-fact-sheet.js
+++ app/models/country-fact-sheet.js
@@ -4,9 +4,9 @@ export default DS.Model.extend({
   title: DS.attr('string'),
   content_html: DS.attr('string'),
   published_date: DS.attr('string'),
+  source: DS.attr('string'),
 
   country: DS.attr('array'),
-  source: DS.attr('array'),
   topic: DS.attr('array'),
 
   searchResultHeader: function() {
