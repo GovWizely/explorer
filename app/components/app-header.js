e13527267bffdef0dd75f6077fb42d0494f00a26
AJ Singh <ajsingh273@gmail.com>
2015-12-07 10:32:30 -0500

    new navigation and styles

--- app/components/app-header.js
+++ app/components/app-header.js
@@ -4,13 +4,17 @@ import ENV from 'explorer/config/environment';
 export default Ember.Component.extend({
   developerportalUrl: ENV.developerportalUrl,
   apiKeyUrl: ENV.apiKeyUrl,
-  availableApisNavItems: [
-      { text: 'Market Research Library', path:     'market-research-library.html' },
+  webservices_baseurl: 'https://api.govwizely.com',
+  webservices_baseurl_users_signup: 'https://api.govwizely.com/users/sign_up',
+  topApisNavItems: [
       { text: 'Consolidated Screening List', path: 'consolidated-screening-list.html' },
-      { text: 'Trade Events', path:                'trade-events.html' },
-      { text: 'Trade Leads', path:                 'trade-leads.html' },
-      { text: 'Tariff Rates', path:                'tariff-rates.html' },
-      { text: 'FAQs on Exporting', path:           'faqs-exporting.html' },
-      { text: 'List of all APIs', path:            'api-list.html' }
-    ]
+      { text: 'Trade Events', path: 'trade-events.html' },
+      { text: 'Trade Leads', path: 'trade-leads.html' },
+      { text: 'Tariff Rates', path: 'tariff-rates.html' },
+      { text: 'De Minimis', path: 'de-minimis.html' }
+    ],
+  apiResourcesNavItems: [
+      { text: 'List of all APIs', path: 'api-list.html' },
+      { text: 'Search Widgets', path: 'search-widgets.html' }
+  ]
 });
